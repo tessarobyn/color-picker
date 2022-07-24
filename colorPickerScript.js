6c97a3cded1a714b28448134f163648f4af77af3
Tessa Grainger <tessa.robyn@outlook.com>
2022-07-24 12:21:50 +0100

    Set up new files and imported colorConversions

--- colorPickerScript.js
+++ colorPickerScript.js
@@ -1,29 +1,4 @@
-function hsvToRgb(h, s, v) {
-  s = s / 100;
-  v = v / 100;
-  const c = v * s;
-  const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
-  const m = v - c;
-  let arr;
-  if (0 <= h && h < 60) {
-    arr = [c, x, 0];
-  } else if (60 <= h && h < 120) {
-    arr = [x, c, 0];
-  } else if (120 <= h && h < 180) {
-    arr = [0, c, x];
-  } else if (180 <= h && h < 240) {
-    arr = [0, x, c];
-  } else if (240 <= h && h < 300) {
-    arr = [x, 0, c];
-  } else if (300 <= h && h < 360) {
-    arr = [c, 0, x];
-  }
-  const rgb = [];
-  rgb.push(Math.round((arr[0] + m) * 255));
-  rgb.push(Math.round((arr[1] + m) * 255));
-  rgb.push(Math.round((arr[2] + m) * 255));
-  return rgb;
-}
+import { hsvToRgb } from "/colorConversions.js";
 
 function getOffset(canvas) {
   const rect = canvas.getBoundingClientRect();
@@ -32,8 +7,8 @@ function getOffset(canvas) {
 
 function mouseDown(event, canvas) {
   const pos = getOffset(canvas);
-  mouseX = event.clientX - pos[0];
-  mouseY = event.clientY - pos[1];
+  const mouseX = event.clientX - pos[0];
+  const mouseY = event.clientY - pos[1];
   return [mouseX, mouseY];
 }
 
@@ -633,13 +608,16 @@ console.log(
   container.style.height.slice(0, container.style.height.length - 2)
 );
 const components = ["colorBar", "hex", "rgb", "hsl", "hsv", "cmyk"];
-colorPicker = new ColorPicker(
+const colorPicker = new ColorPicker(
   container,
   container.style.width.slice(0, container.style.width.length - 2),
   container.style.height.slice(0, container.style.height.length - 2),
   components
 );
 
+let updateHueSlider;
+let updateTransparencySlider;
+let updatePointer;
 colorPicker.canvas.addEventListener("mousedown", (event) => {
   colorPicker.hueBar.slider.drag(event);
   if (colorPicker.transparencyBar) {
@@ -647,7 +625,7 @@ colorPicker.canvas.addEventListener("mousedown", (event) => {
   }
   colorPicker.mainScreen.colorPointer.drag(event);
 });
-colorPicker.canvas.addEventListener("mousemove", (event) => {
+window.addEventListener("mousemove", (event) => {
   if (colorPicker.hueBar.slider.dragging) {
     updateHueSlider = window.requestAnimationFrame(function () {
       colorPicker.hueBar.slider.update(event);
